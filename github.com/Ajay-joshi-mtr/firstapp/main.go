fb480dd6b3c6ce2008f34e376d252a70b68d9ee9
Ajay joshi <ajayjoshi.mtr@gmail.com>
2022-01-24 12:36:14 +0530

    bool integers float string rune and operators

--- github.com/Ajay-joshi-mtr/firstapp/main.go
+++ github.com/Ajay-joshi-mtr/firstapp/main.go
@@ -1,22 +1,41 @@
 package main
 
-import "fmt"
-
-var (
-	name   string  = "ajay"
-	mobile int     = 45454545
-	info   string  = "info"
-	j      float32 = 25.5
+import (
+	"fmt"
 )
-var I int = 50 // exported if capitalize in whole application
 
 func main() {
-	i := 55
-	var j float32 = 35.5
-	k := float32(i) //explicit type conversion
-	fmt.Printf("%v, %T\n", name, name)
-	fmt.Printf("%v, %T\n", mobile, mobile)
-	fmt.Printf("%v, %T\n", k, k)
-	fmt.Printf("%v, %T\n", j, j)
-	fmt.Printf("%v, %T\n", I, I)
+	//all variables inilise with zero value
+
+	i := 5
+	j := 5
+	//arithmetic
+	fmt.Println(i + j)
+	fmt.Println(i - j)
+	fmt.Println(i * j)
+	fmt.Println(i / j)
+	i = 10 //1010
+	j = 3  //0011
+	//bitwise
+	fmt.Println(i & j)  //0010
+	fmt.Println(i | j)  //1011
+	fmt.Println(i ^ j)  //1001
+	fmt.Println(i &^ j) //1000
+	//bitshft
+	a := 8              //2^3
+	fmt.Println(a << 2) //0010 2^3 * 2^2 = 2^5
+	fmt.Println(a >> 2) //0010 2^3 / 2^2 = 2^1
+	//strings UTF-8
+	s1 := "hello world in go!" //aliases for bytes and immutable
+	s2 := " Ajay joshi!"
+	//can't be modify s2[0] = "A"
+	fmt.Println(s1 + s2)
+	b := []byte(s1) //uint8
+	fmt.Println(b)
+	//rune UTF-32
+	c := 'a'         //int32 true type alias rune
+	var d rune = 'a' //int32 true type alias rune
+	fmt.Printf("%v , %T\n", c, c)
+	fmt.Printf("%v , %T\n", d, d)
+
 }
