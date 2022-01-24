6b8d63da6941e10fa049d79f668aeb590ee10908
Ajay joshi <ajayjoshi.mtr@gmail.com>
2022-01-24 12:04:52 +0530

    declaration typecasting scope var_group

--- github.com/Ajay-joshi-mtr/firstapp/main.go
+++ github.com/Ajay-joshi-mtr/firstapp/main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+var (
+	name   string  = "ajay"
+	mobile int     = 45454545
+	info   string  = "info"
+	j      float32 = 25.5
+)
+var I int = 50 // exported if capitalize in whole application
+
 func main() {
-	var i int
-	k := 50
-	fmt.Println(k)
-	fmt.Println(i)
+	i := 55
+	var j float32 = 35.5
+	k := float32(i) //explicit type conversion
+	fmt.Printf("%v, %T\n", name, name)
+	fmt.Printf("%v, %T\n", mobile, mobile)
+	fmt.Printf("%v, %T\n", k, k)
+	fmt.Printf("%v, %T\n", j, j)
+	fmt.Printf("%v, %T\n", I, I)
 }
